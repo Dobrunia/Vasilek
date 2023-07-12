989b20b6b334b1c8b131dfe23fe420e2d9c3f700
Dobrunia <dobkos@hotmail.com>
2023-07-12 05:10:51 +0300

    slider

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-import './style.scss';
+import "./style.scss";
+
+//const slider = document.getElementById("slider");
+// setInterval(() => {
+//   if (slider !== null) {
+//     const currentTranslateX = parseInt(slider.style.transform.split("(")[1]) || 0;
+//     slider.style.transform = `translateX(${currentTranslateX - 400}px)`;
+//   }
+// }, 3000);
+
